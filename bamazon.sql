d828fa93fe34facc0805a8ac78185da4e5f1f848
Bessy Gee <nakiwarai13@gmail.com>
2019-12-12 13:44:58 -0500

    added departments table to schema

--- bamazon.sql
+++ bamazon.sql
@@ -8,28 +8,46 @@ product_name VARCHAR(100) NOT NULL,
 department_name VARCHAR(100) NOT NULL,
 price DECIMAL(10,2),
 stock_quantity INT,
+product_sales DECIMAL(10,2) NOT NULL,
 PRIMARY KEY (id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Watchmen (2019 Edition)", "Books", 14.99, 50);
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Nike Women's Ebernon Low Sneaker", "Clothing, Shoes & Jewelry", 48.75, 63);
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Blizzard Bay Men's Ugly Christmas Sweater Arms Too Short T-rex", "Clothing, Shoes & Jewelry", 27.99, 32);
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Bose QuietComfort 35 II Wireless Bluetooth Headphones", "Electronics", 279.00, 100);
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Devacurl No-Poo Original Cleanser, 32oz", "Beauty & Personal Care", 27.00, 87);
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Makeup Setting Spray with Organic Green Tea", "Beauty & Personal Care", 12.25, 265);
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Mike’s Hot Honey, 12 oz Squeeze Bottle", "Grocery & Gourmet Food", 8.48, 113);
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Professor Phardtpounders Colon Cleaner Hot Sauce", "Grocery & Gourmet Food", 9.95, 59);
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("KitchenAid 5-Qt. Stand Mixer", "Home & Kitchen", 429.99, 33);
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Nespresso Original Espresso Machine", "Home & Kitchen", 164.99, 52);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Watchmen (2019 Edition)", "Books", 14.99, 50, 0);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Nike Women's Ebernon Low Sneaker", "Clothing, Shoes & Jewelry", 48.75, 63, 0);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Blizzard Bay Men's Ugly Christmas Sweater Arms Too Short T-rex", "Clothing, Shoes & Jewelry", 27.99, 32, 0);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Bose QuietComfort 35 II Wireless Bluetooth Headphones", "Electronics", 279.00, 100, 0);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Devacurl No-Poo Original Cleanser, 32oz", "Beauty & Personal Care", 27.00, 87, 0);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Makeup Setting Spray with Organic Green Tea", "Beauty & Personal Care", 12.25, 265, 0);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Mike’s Hot Honey, 12 oz Squeeze Bottle", "Grocery & Gourmet Food", 8.48, 113, 0);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Professor Phardtpounders Colon Cleaner Hot Sauce", "Grocery & Gourmet Food", 9.95, 59, 0);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("KitchenAid 5-Qt. Stand Mixer", "Home & Kitchen", 429.99, 33, 0);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Nespresso Original Espresso Machine", "Home & Kitchen", 164.99, 52, 0);
 
-SELECT * FROM products;
+CREATE TABLE departments (
+department_id INT AUTO_INCREMENT UNIQUE,
+department_name VARCHAR(100) NOT NULL,
+over_head_costs DECIMAL(10,2),
+total_sales DECIMAL(10,2),
+total_profit DECIMAL(10,2),
+PRIMARY KEY (department_id)
+);
+
+INSERT INTO departments (department_name, over_head_costs)
+VALUES ('Books', 500)
+, ('Clothing, Shoes & Jewelry', 3254)
+, ('Electronics', 20000)
+, ('Beauty & Personal Care', 4337)
+, ('Grocery & Gourmet Food', 1032)
+, ('Home & Kitchen', 16400);
+
+drop table departments;
